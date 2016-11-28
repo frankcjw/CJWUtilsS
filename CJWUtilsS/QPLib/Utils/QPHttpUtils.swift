77498e76178cfaf291a23d00ffb1b50251e3a69e
cjw <fk911c@gmail.com>
2016-11-28 15:12:59 +0800

    d

--- CJWUtilsS/QPLib/Utils/QPHttpUtils.swift
+++ CJWUtilsS/QPLib/Utils/QPHttpUtils.swift
@@ -611,10 +611,8 @@ public extension QPHttpUtils {
      */
 	public class func IDCardRecongize(image: UIImage, success: QPSuccessBlock, failure: QPFailBlock) {
 
-//		let url = "https://api.megvii.com/cardpp/v1/ocridcard"
-		let url = "http://qp.cenjiawen.com:9090/qp/cardRecg"
-		let param = ["api_key": "4KIL18K6lUgz3YZdZtBcwUuedBudMJE4", "api_secret": "2Gdg9sMUo3aUNytMPbshWsnsrxIDEf2M"]
-		QPHttpUtils.sharedInstance.uploadImage(url, param: param, imageName: ["image_file"], images: [image], success: { (response) in
+		let url = "http://web.cenjiawen.com/qp/cardRecg"
+		QPHttpUtils.sharedInstance.uploadImage(url, param: nil, imageName: ["image_file"], images: [image], success: { (response) in
 			success(response: response)
 		}) {
 			failure()
