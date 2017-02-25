d0e3a15f3aa06b27fcdab5d99b8b299caf5fcd26
cjw <fk911c@gmail.com>
2017-02-25 17:41:06 +0800

    add QPNormalTableViewCell

--- CJWUtilsS/QPLib/UI/QPBaseTableViewCell.swift
+++ CJWUtilsS/QPLib/UI/QPBaseTableViewCell.swift
@@ -234,3 +234,93 @@ public class QPConfirmTableViewCell: QPTableViewCell {
 	}
 
 }
+
+// MARK: - QPNormalTableViewCell
+public class QPNormalTableViewCell: UITableViewCell {
+	/// 父view controller
+	public var rootViewController: UIViewController?
+	/// 这个cell的indexPath
+	public var indexPath: NSIndexPath?
+	public var didSetupConstraints = false
+	/// cell的数据
+	public var cellInfo = NSDictionary()
+
+	required public init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		initCell()
+	}
+
+	/**
+     为contentView添加autoLayout
+     */
+	func setupAutoLayout() {
+		self.contentView.setToAutoLayout()
+		contentView.alignLeading("0", trailing: "0", toView: self)
+	}
+
+	override public init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		initCell()
+	}
+
+	/**
+     初始化cell
+     
+     - returns: nil
+     */
+	func initCell() {
+		setupViews(contentView)
+		// setupAutoLayout()
+		self.selectionStyle = UITableViewCellSelectionStyle.None
+	}
+
+	/**
+     更新Constrains
+     */
+	override public func updateConstraints() {
+
+		// if !didSetupConstraints {
+		// setupConstrains(contentView)
+		// didSetupConstraints = true
+		// }
+		setupConstrains(contentView)
+
+		super.updateConstraints()
+	}
+
+	/**
+     构造Constrains
+     
+     - parameter view: cell.contentView
+     */
+	public func setupConstrains(view: UIView) {
+	}
+
+	/**
+     初始化cell内的view
+     
+     - parameter view: cell.contentView
+     */
+	public func setupViews(view: UIView) {
+	}
+
+	/**
+     添加cell内容
+     
+     - parameter info: info
+     */
+	public func setInfo(info: NSDictionary) {
+		self.cellInfo = info
+		setupConstrains(contentView)
+	}
+
+	public func setJson(json: JSON) {
+		setupConstrains(contentView)
+	}
+
+	public func setup() {
+		self.setNeedsUpdateConstraints()
+		self.updateConstraintsIfNeeded()
+	}
+}
+
